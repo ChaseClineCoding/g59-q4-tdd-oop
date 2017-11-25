6e0602b981f3acbf178dc857b69441e681af10e9
Chase (Ryan Cline) <ryancline88@yahoo.com>
2017-11-25 13:34:47 -0700

    Finished cars and started dmv

--- cars/car.js
+++ cars/car.js
@@ -1,9 +1,18 @@
 module.exports = Car
 
 function Car(mpg) {
-
+  this.gallons = 0
+  this.mpg = mpg
+  this.odometer = 0
+  this.trips = []
 }
 
 Car.prototype.fill = function(gallons) {
+  this.gallons += gallons
+}
 
+Car.prototype.drive = function(miles) {
+  this.gallons -= miles / this.mpg
+  this.odometer += miles
+  this.trips.push(miles + ' miles')
 }
--- dmv/dmv.js
+++ dmv/dmv.js
@@ -1,7 +1,23 @@
 function DMV(agents) {
+  this.agents = agents
+  this.customers = []
+  // this.customer = null
 };
 
-DMV.prototype.customersInLine = function () {
+DMV.prototype.customersInLine = function() {
+  return this.customers
 };
+
+DMV.prototype.enter = function(customer) {
+  this.customers.push(customer)
+}
+
+DMV.prototype.currentCustomerFor = function(agent) {
+ return this.customers[0]
+}
+
+DMV.prototype.nextCustomer = function() {
+  this.customer = this.customers.shift()
+}
 
 module.exports = DMV;
